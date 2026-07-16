d7eb7cdd7b9a63c3148500f705ed874674610783
Shaurya Kapoor <shauryakapoor.work@gmail.com>
2026-07-16 10:03:52 +0530

    Added LinkedList solutions

---
+++ LinkedList/InsertAfterNode.java
@@ -0,0 +1,12 @@
+public class InsertAfterNode {
+
+    static void insertAfter(Node current, int trackId) {
+
+        if (current == null) return;
+
+        Node newNode = new Node(trackId);
+        newNode.next = current.next;
+        current.next = newNode;
+    }
+
+}
