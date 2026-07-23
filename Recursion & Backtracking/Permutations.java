343d6805b50f5b3f9787f5c3ebfffbd787b90f78
Shaurya Kapoor <shauryakapoor.work@gmail.com>
2026-07-23 10:29:48 +0530

    Completed Backtracking Assignment

---
+++ Recursion & Backtracking/Permutations.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Permutations {
+
+    public List<List<String>> generateSchedules(String[] engineers) {
+
+        List<List<String>> result = new ArrayList<>();
+
+        boolean[] used = new boolean[engineers.length];
+
+        backtrack(engineers, used, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backtrackString[] engineers, boolean[] used, List<String> path,List<List<String>> result) {
+
+        if (path.size() == engineers.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < engineers.length; i++) {
+
+            if (used[i])
+                continue;
+
+            used[i] = true;
+            path.add(engineers[i]);
+
+            backtrack(engineers, used, path, result);
+
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Permutations obj = new Permutations();
+        String[] engineers = {"A", "B", "C"};
+        System.out.println(obj.generateSchedules(engineers));
+    }
+}
